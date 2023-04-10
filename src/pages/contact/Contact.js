003fa6096ef6019008d1562c49128cd3dd28c852
ferax <ferbin99@gmail.com>
2023-04-10 00:30:47 -0600

    code refactor

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,40 +1,40 @@
 import Icons from "../../utils/TechIcons";
 import "./Contact.scss";
 const Contact = () => {
-	return (
-		<section className="contact">
-			<h1 className="contact__title">Contact Me .</h1>
-			<div className="contact__buttons">
-				<a href="https://github.com/ferax99">
-					<img
-						src={Icons["git"]}
-						alt="git"
-						className="contact__buttons__button"
-					/>
-				</a>
-				<a href="https://www.fiverr.com/ferax99?public_mode=true">
-					<img
-						src={Icons["fiverr"]}
-						alt="fiverr"
-						className="contact__buttons__button"
-					/>
-				</a>
-				<a href="https://us11.list-manage.com/contact-form?u=8b3c8a6fcafe30907def2005d&form_id=8f3692c6e4c5cb142d89638f2744bdf4">
-					<img
-						src={Icons["mail"]}
-						alt="mail"
-						className="contact__buttons__button"
-					/>
-				</a>
-				<a href="https://www.linkedin.com/in/fernando-b-9298721b3/">
-					<img
-						src={Icons["linkedin"]}
-						alt="linkedin"
-						className="contact__buttons__button"
-					/>
-				</a>
-			</div>
-		</section>
-	);
+  return (
+    <section className="contact">
+      <h1 className="contact__title">Contact Me .</h1>
+      <div className="contact__buttons">
+        <a href="https://github.com/ferax99">
+          <img
+            src={Icons["git"]}
+            alt="git"
+            className="contact__buttons__button"
+          />
+        </a>
+        <a href="https://www.fiverr.com/ferax99?public_mode=true">
+          <img
+            src={Icons["fiverr"]}
+            alt="fiverr"
+            className="contact__buttons__button"
+          />
+        </a>
+        <a href="https://us11.list-manage.com/contact-form?u=8b3c8a6fcafe30907def2005d&form_id=8f3692c6e4c5cb142d89638f2744bdf4">
+          <img
+            src={Icons["mail"]}
+            alt="mail"
+            className="contact__buttons__button"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/fernando-b-9298721b3/">
+          <img
+            src={Icons["linkedin"]}
+            alt="linkedin"
+            className="contact__buttons__button"
+          />
+        </a>
+      </div>
+    </section>
+  );
 };
 export default Contact;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,41 +1,54 @@
-import './Home.scss';
-import logo from '../../assets/shadow.png';
-import { useEffect, useState } from 'react';
+import "./Home.scss";
+import logo from "../../assets/shadow.png";
+import { useEffect, useState } from "react";
+
+const catchyPhrases = [
+  `'I like programming'`,
+  `'light attracts bugs'`,
+  `'not a Bug ... is a feature'`,
+  `'A machine that turns coffee into code'`,
+  `'light attracts bugs'`,
+  `'Don't test your code in production, use mockito ;)'`,
+  `'The best thing about a boolean is even if you are wrong, you are only off by a bit'`,
+  `'ChatGpt also made this caption'`,
+  `'Sometimes i use glasses cuase i can't C#'`,
+];
 
 const Home = () => {
-  const [slogan,setSlogan] = useState(`'I like programming'`);
-
+  const [slogan, setSlogan] = useState(`'I like programming'`);
 
   useEffect(() => {
-    let index= 0;
-    const interval =  setInterval(() => {
-      const catchyPhrases = [`'I like programming'`, `Coffe at 11pm`,`not a Bug ... is a feature`,`react > angular? 1:0`];
+    let index = 0;
+    const interval = setInterval(() => {
       index++;
-      setSlogan(catchyPhrases[Math.floor(Math.random() * catchyPhrases.length)]);
-      if(index>8){clearInterval(interval)};
+      setSlogan(
+        catchyPhrases[Math.floor(Math.random() * catchyPhrases.length)]
+      );
+      if (index > 8) {
+        clearInterval(interval);
+      }
     }, 2500);
-  
-    return () => clearInterval(interval) ;
 
+    return () => clearInterval(interval);
   }, []);
-
+  const block = "home";
 
   return (
     <section>
-
-    <div className="screen1">
-    <h1 className="nombre">Fernando Binda</h1>
-      <div className="info">
-
-        <img className="logo move" src={logo} alt="description" />
-        <h1>Full Stack Developer </h1>
-        <p >{slogan}</p>
+      <div className={`${block}`}>
+        <h1 className={`${block}__title`}>Fernando Binda</h1>
+        <div className={`${block}__container`}>
+          <img
+            className={`${block}__container__logo`}
+            src={logo}
+            alt="description"
+          />
+          <h1>Full Stack Developer </h1>
+          <p className={`${block}__container__description`}>{slogan}</p>
+        </div>
       </div>
-     
-    </div>
-    
     </section>
   );
-}
+};
 
 export default Home;
