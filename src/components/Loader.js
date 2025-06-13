6eea346d2164a55ee8af1255e0fee0807cff7855
Fernando <ferbin99@gmail.com>
2025-06-13 16:50:38 -0600

    code fix for prod

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,8 +4,8 @@ import { Loader2 } from 'lucide-react'; // Icono de cargador
 // Loader simplificado: solo muestra el spinner
 const Loader = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-center h-full w-full">
+      <Loader2 className="animate-spin h-16 w-16 text-[#FF8800]" aria-label="Loading content" />
     </div>
   );
 };
